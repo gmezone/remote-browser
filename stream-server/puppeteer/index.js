88dae392074e30bfd0b8154492677984c3dfaf62
zvirot1 <zvirot@gmail.com>
2023-09-27 18:14:55 +0300

    Update index.js

--- stream-server/puppeteer/index.js
+++ stream-server/puppeteer/index.js
@@ -24,6 +24,7 @@ export default async (options) => {
 
   const browser = await puppeteer.launch({
     args: [
+      //7777
       '--block-new-web-contents',
       '--disable-breakpad',
       '--disable-dev-shm-usage',
