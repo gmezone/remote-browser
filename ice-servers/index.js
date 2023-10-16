2d90d2e3cc4c6637039985b6ed3524a03eef2f8a
zvirot1 <zvirot@gmail.com>
2023-10-16 15:50:30 +0300

    Update index.js

--- ice-servers/index.js
+++ ice-servers/index.js
@@ -3,6 +3,8 @@ import crypto from 'crypto'
 const sServer = process.env.STUN_SERVER
 const tServer = process.env.TURN_SERVER
 const key = process.env.TURN_KEY
+const envUserName = process.env.TURN_USER
+const envCredential = process.env.TURN_PASS
 
 const expiration = () => {
   const microsecond = +new Date
@@ -30,17 +32,28 @@ export default () => {
     servers.push({ urls: `stun:${sServer}` })
   }
 
-  if (tServer && key) {
-    // const username = `${expiration()}:${random()}`
-    const username = '729553bf783b72cad075a2dc'
-    console.log(username)
-    const credential = '6exlyocIM8tYj2aB'
+  if (tServer && envUserName && envCredential) {
+     const username = envUserName
+    const credential = envCredential
 
     servers.push({
       urls: `turn:${tServer}`,
       username: username,
-      // credential: hmac(username)
       credential: credential
+    })
+  }
+
+  if (tServer && key) {
+     const username = `${expiration()}:${random()}`
+    //const username = '729553bf783b72cad075a2dc'
+    console.log(username)
+    //const credential = '6exlyocIM8tYj2aB'
+
+    servers.push({
+      urls: `turn:${tServer}`,
+      username: username,
+      credential: hmac(username)
+   //   credential: credential
     })
   }
     console.log(servers)
